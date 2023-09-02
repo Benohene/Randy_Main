258f79384d8baf9cccf5b83499582b5f3b785666
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-09-02 22:44:58 +0200

    Added the search and sort logic to the user profile

--- booking/views.py
+++ booking/views.py
@@ -1,13 +1,12 @@
 '''booking views'''
 from datetime import datetime
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from .models import Appointment
 from .forms import AppointmentForm
 from django.contrib import messages
-from django.core.mail import send_mail
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
-from django.contrib.admin.views.decorators import staff_member_required
+from django.db.models import Q
 
 
 def index(request):
@@ -25,13 +24,16 @@ def contact(request):
     return render(request, 'contact.html')
 
 
-from django.contrib.auth.decorators import login_required
-
 @login_required
 def create_appointment(request):
     '''create appointment view'''
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
+
+        # check if user is authenticated
+        if not request.user.is_authenticated:
+            messages.error(request, 'Please login to book an appointment.')
+            return redirect('create_appointment')
 
         if form.is_valid():
             # Check if the selected time slot is already booked for the selected day
@@ -64,29 +66,41 @@ def create_appointment(request):
     return render(request, 'create_appointment.html', {'form': form})
 
 
-
-# def appointment_list(request):
-#     '''appointment list view'''
-#     appointments = Appointment.objects.all()
-#     paginator = Paginator(appointments, 3)
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-
-#     template = 'appointment_list.html'
-#     context = {
-#         'appointments': page_obj,
-#     }
-#     return render(request, template, context)
-
-
-
 @login_required
 def appointment_list(request):
     '''appointment list view'''
+    #display nothing in sort by and search by
+
+    # Get the search query from the request
+    search_query = request.GET.get('q', '')
+
+    # Initialize the appointment queryset based on user type
     if request.user.is_superuser:
         appointments = Appointment.objects.all()
     else:
         appointments = Appointment.objects.filter(customer=request.user)
+
+    # Apply search filter if a search query is provided
+    if search_query:
+        appointments = appointments.filter(
+            Q(customer__username__icontains=search_query) |
+            Q(name__icontains=search_query) |
+            Q(phone_number__icontains=search_query) |
+            Q(message__icontains=search_query) |
+            Q(appointment_date__icontains=search_query) |
+            Q(appointment_time__icontains=search_query)
+        )
+
+    # Sorting logic based on the 'sort' parameter
+    sort_by = request.GET.get('sort', '')
+    if sort_by == 'date_asc':
+        appointments = appointments.order_by('appointment_date', 'appointment_time')
+    elif sort_by == 'date_desc':
+        appointments = appointments.order_by('-appointment_date', '-appointment_time')
+    elif sort_by == 'name_asc':
+        appointments = appointments.order_by('customer__username', 'appointment_date', 'appointment_time')
+    elif sort_by == 'name_desc':
+        appointments = appointments.order_by('-customer__username', '-appointment_date', '-appointment_time')
 
     paginator = Paginator(appointments, 3)
     page_number = request.GET.get('page')
@@ -95,5 +109,7 @@ def appointment_list(request):
     template = 'appointment_list.html'
     context = {
         'appointments': page_obj,
+        'search_query': search_query,
+        'sort_by': sort_by,
     }
     return render(request, template, context)

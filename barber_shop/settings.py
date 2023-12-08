f9a61733c8c93f97a322c6d6dc80d82ed76e2243
Benjamin Ohene Konadu <benohenekonadu@outlook.com>
2023-12-08 20:12:36 +0100

    added database url

--- barber_shop/settings.py
+++ barber_shop/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,6 +113,9 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
+
 
 
 # Password validation
